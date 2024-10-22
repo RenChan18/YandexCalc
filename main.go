6627a0ab160b803987300e8182ddf2c2c6a015e0
Renata <renata.talacheva@yandex.ru>
2024-10-22 22:34:34 +0300

    first variant test done

--- main.go
+++ main.go
@@ -1,11 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
 
-func Calc(expression string) (float64, error) {
-	return 0, nil
+type Operator rune
+
+func (op Operator) IsOperator() bool {
+	switch op {
+	case '+', '-', '*', '/':
+		return true
+	default:
+		return false
+	}
 }
 
-func main () {
+func Calc(expression string) (float64, error) {
+	var numbers []float64
+	var operator Operator
+	var num string
+
+	for _, symb := range expression {
+		if Operator(symb).IsOperator() {
+			operator = Operator(symb)
+			f, err := strconv.ParseFloat(num, 64)
+			if err != nil {
+				return 0, errors.New("invalid number")
+			}
+			numbers = append(numbers, f)
+			num = ""
+		} else {
+			num += string(symb)
+		}
+	}
+
+	if num != "" {
+		f, err := strconv.ParseFloat(num, 64)
+		if err != nil {
+			return 0, errors.New("invalid number")
+		}
+		numbers = append(numbers, f)
+	}
+
+	if len(numbers) != 2 {
+		return 0, errors.New("invalid expression")
+	}
+
+	switch operator {
+	case '+':
+		return numbers[0] + numbers[1], nil
+	case '-':
+		return numbers[0] - numbers[1], nil
+	case '*':
+		return numbers[0] * numbers[1], nil
+	case '/':
+		if numbers[1] == 0 {
+		return 0, errors.New("division by zero")
+	}
+		return numbers[0] / numbers[1], nil
+	default:
+		return 0, errors.New("unknown operator")
+	}
+}
+
+func main() {
 	fmt.Println("Hello")
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,32 @@
 package main
 
-import "testing"
+import (
+	//"errors"
+	"testing"
+)
+
+type Test struct {
+	in string
+	result float64
+	exp_err error
+}
+
+var tests = []Test{
+	{"2+3", 5.0, nil},
+	//{"2/0", 0.0, errors.New("division by zero")},
+	{"2*3", 6.0, nil},
+	{"3*0", 0.0, nil},
+	{"3/2", 1.5, nil},
+}
 
 func TestCalc(t *testing.T) {
-	got := ""
-	want := 0, nil
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	for i, test := range tests {
+		got, err := Calc(test.in)
+		if got != test.result {
+			t.Errorf("#%d: got %v want %v", i,  got, test.result)
+		} else if err != test.exp_err {
+			t.Errorf("#%d: got %v want %v", i,  err, test.exp_err)
+		}
 	}
+	
 }
